0025cde4e89d5ae64c0f898b4359065e70d22538
saway <sawa.y918@gmail.com>
2024-10-13 21:14:20 +0900

    fix: implement again StudentConverter, StudentDetail.java, StudentDetails.html

---
+++ src/main/java/reisetech/StudentManagement/domain/StudentDetail.java
@@ -0,0 +1,15 @@
+package reisetech.StudentManagement.domain;
+
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+import reisetech.StudentManagement.data.Student;
+import reisetech.StudentManagement.data.StudentsCourses;
+
+@Getter
+@Setter
+public class StudentDetail {
+
+  private Student student;
+  private List<StudentsCourses> studentsCourses;
+}
