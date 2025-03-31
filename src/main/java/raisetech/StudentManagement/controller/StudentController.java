2caaf767996520b832c02a75592bcc8452284c13
saway <sawa.y918@gmail.com>
2025-03-31 16:17:39 +0900

    feat: 受講生IDに紐づいた受講生詳細検索メソッドを復元

--- src/main/java/raisetech/StudentManagement/controller/StudentController.java
+++ src/main/java/raisetech/StudentManagement/controller/StudentController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,6 +49,13 @@ public class StudentController {
     }
     service.registerStudent(studentDetail);
     return "redirect:/studentList";
+  }
+
+  @GetMapping("/student/{studentId}")
+  public String viewStudent(@PathVariable("studentId") int studentId, Model model) {
+    StudentDetail studentDetail = service.searchStudent(studentId);
+    model.addAttribute("studentDetail", studentDetail);
+    return "updateStudent";
   }
 
   @PostMapping("/updateStudent")
