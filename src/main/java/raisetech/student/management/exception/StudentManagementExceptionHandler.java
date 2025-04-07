84f578c2795955a016732ca728c1de9cfbb7d061
saway <sawa.y918@gmail.com>
2025-04-07 13:02:57 +0900

    refactore: rename ValidationExceptionHandler -> StudentManagementExceptionHandler

---
+++ src/main/java/raisetech/student/management/exception/StudentManagementExceptionHandler.java
@@ -0,0 +1,20 @@
+package raisetech.student.management.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class StudentManagementExceptionHandler {
+
+  @ExceptionHandler(InvalidAccessException.class)
+  public ResponseEntity<String> handleInvalidAccessException(InvalidAccessException ex) {
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+  }
+
+  @ExceptionHandler(NotExistIdException.class)
+  public ResponseEntity<String> handleNotExistIdException(NotExistIdException ex) {
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+  }
+}
