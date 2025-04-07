d021e4595f557ecfbd58641cbd01f55083220d7b
saway <sawa.y918@gmail.com>
2025-04-07 14:49:58 +0900

    feat: ExceptionHandlerクラスにMethodArgumentNotValidExceptionのハンドリングを実装

--- src/main/java/raisetech/student/management/exception/StudentManagementExceptionHandler.java
+++ src/main/java/raisetech/student/management/exception/StudentManagementExceptionHandler.java
@@ -1,7 +1,13 @@
 package raisetech.student.management.exception;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -17,4 +23,26 @@ public class StudentManagementExceptionHandler {
   public ResponseEntity<String> handleNotExistIdException(NotExistIdException ex) {
     return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
   }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<ErrorResponseBody> handleMethodArgumentNotValidException(
+      MethodArgumentNotValidException ex) {
+
+    ErrorResponseBody errorResponseBody =
+        new ErrorResponseBody(HttpStatus.BAD_REQUEST, "validation error", newErrorResponse(ex));
+    return ResponseEntity.badRequest().body(errorResponseBody);
+  }
+  
+  private List<Map<String, String>> newErrorResponse(BindException ex) {
+
+    List<Map<String, String>> errors = new ArrayList<>();
+    ex.getBindingResult().getFieldErrors().forEach(fieldError -> {
+      Map<String, String> error = new HashMap<>();
+      error.put("field", fieldError.getField());
+      error.put("message", fieldError.getDefaultMessage());
+      errors.add(error);
+    });
+    return errors;
+  }
+
 }
