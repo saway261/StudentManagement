74eab99ac2874117631487f7e4ff3054d0974aa9
saway <sawa.y918@gmail.com>
2025-04-07 12:00:21 +0900

    feat: StudentControllerに@Operationを付与してAPI仕様情報(summary,description,responses)を記述

--- src/main/java/raisetech/student/management/controller/StudentController.java
+++ src/main/java/raisetech/student/management/controller/StudentController.java
@@ -1,5 +1,10 @@
 package raisetech.student.management.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import java.util.List;
@@ -33,22 +38,29 @@ public class StudentController {
     this.service = service;
   }
 
-  /**
-   * アクティブ受講生詳細の一覧検索です。 全件検索を行うので、条件指定は行いません。
-   *
-   * @return アクティブ受講生詳細一覧
-   */
+  @Operation(
+      summary = "アクティブな受講生詳細一覧の検索",
+      description = "アクティブな受講生詳細の一覧を検索します。全件検索を行うので、条件指定はしません",
+      responses = {@ApiResponse(
+          content = @Content(mediaType = "application/json",
+              array = @ArraySchema(schema = @Schema(implementation = StudentDetail.class))
+          )
+      )}
+  )
   @GetMapping("/students")
   public List<StudentDetail> getActiveStudentDetailList() {
     return service.searchActiveStudentDetailList();
   }
 
-  /**
-   * 受講生検索です。キャンセルの有無を問わず、受講生の全件からIDに紐づく任意の受講生の詳細を取得します。
-   *
-   * @param studentId 受講生ID
-   * @return 受講生詳細
-   */
+  @Operation(
+      summary = "受講生詳細検索",
+      description = "アクティブ・非アクティブを問わず、受講生詳細の全件から受講生IDが一致する受講生の詳細を取得します。",
+      responses = {@ApiResponse(
+          content = @Content(mediaType = "application/json",
+              array = @ArraySchema(schema = @Schema(implementation = StudentDetail.class))
+          )
+      )}
+  )
   @GetMapping("/students/{studentId}")
   public StudentDetail viewStudentDetail(@PathVariable("studentId") @Positive int studentId)
       throws NotExistIdException {
@@ -59,18 +71,28 @@ public class StudentController {
     }
   }
 
+  @Operation(
+      summary = "アクティブな受講生詳細一覧の検索(旧ver・非使用)",
+      description = "現在使われていないURIです。エラーを返し、正しいアドレスへのアクセスを促します",
+      responses = {
+          @ApiResponse(responseCode = "404", description = "not found")
+      }
+  )
   @GetMapping("/studentAndCourses")
   public ResponseEntity<StudentDetail> pastGetStudentDetails() throws InvalidAccessException {
     throw new InvalidAccessException(
         "現在無効なURLです。受講生一覧を見るには /studentList にアクセスしてください。");
   }
 
-  /**
-   * 受講生詳細の登録を行います。
-   *
-   * @param studentDetail 受講生詳細
-   * @return 実行結果
-   */
+  @Operation(
+      summary = "受講生詳細登録",
+      description = "受講生の登録を行います",
+      responses = {@ApiResponse(
+          content = @Content(mediaType = "application/json",
+              array = @ArraySchema(schema = @Schema(implementation = StudentDetail.class))
+          )
+      )}
+  )
   @PostMapping("/students")
   @Validated(OnCreate.class)
   public ResponseEntity<StudentDetail> registerStudent(
@@ -86,6 +108,18 @@ public class StudentController {
    * @param studentDetail
    * @return 実行結果
    */
+  @Operation(
+      summary = "受講生詳細更新",
+      description = "受講生詳細の更新を行います。キャンセルフラグの更新(アクティブ⇔非アクティブ)もここで行います。受講生IDが登録されていない、または受講生コースIDがひとつでも受講生IDと紐づかない場合はエラーを返します。",
+      responses = {
+          @ApiResponse(
+              content = @Content(mediaType = "application/json",
+                  array = @ArraySchema(schema = @Schema(implementation = StudentDetail.class)))),
+          @ApiResponse(
+              responseCode = "400", description = "id not exist"
+          )
+      }
+  )
   @PutMapping("/students")
   @Validated(OnUpdate.class)
   public ResponseEntity<StudentDetail> updateStudent(
