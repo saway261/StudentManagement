7b1454385a7f4fac7fcff76965e5e87de7b87ae5
saway <sawa.y918@gmail.com>
2025-04-20 17:37:20 +0900

    feat: StudentCourseクラスにおいて、受講生詳細登録処理と受講生詳細更新それぞれで呼び出されるコンストラクタを定義

--- src/main/java/raisetech/student/management/data/StudentCourse.java
+++ src/main/java/raisetech/student/management/data/StudentCourse.java
@@ -35,16 +35,48 @@ public class StudentCourse {
   private LocalDate courseStartAt;
 
   @Schema(description = "コース終了予定日 コース開始日の6か月後の日付", example = "2025-07-01")
+  @NotNull(groups = OnUpdate.class)
   private LocalDate courseEndAt;
 
-
+  /**
+   * 受講生詳細登録で呼び出されるコンストラクタです。コース名,受講生IDを受け取り、コンストラクタが呼び出された日付をもとに、受講開始日と受講終了予定日を自動的にセットします。
+   * 受講生詳細更新で呼び出すと、courseIdが0なのでのちの処理でInvalidIdExceptionが投げられます。これにより、受講生詳細更新で子のコンストラクタを呼び出し、意図せず受講終了予定日が書き替えられることを防ぎます。
+   *
+   * @param courseName コース名
+   * @param studentId  受講生ID
+   */
   public StudentCourse(String courseName, int studentId) {
+    this.courseId = 0;
     this.courseName = courseName;
     this.studentId = studentId;
 
     LocalDate now = LocalDate.now();
     this.courseStartAt = now;
     this.courseEndAt = now.plusMonths(6);
+  }
+
+  /**
+   * 受講生詳細更新で呼び出されるコンストラクタです。受講生コースオブジェクトと受講生IDを受け取ります。
+   * 受講生IDが0またはコース名,コース終了予定日がnullのStudentCourseオブジェクトを受け取った場合はNullPointerExceptionを投げます。
+   *
+   * @param requestCourse 受講生コースコースオブジェクト
+   * @param studentId     受講生ID
+   */
+  public StudentCourse(StudentCourse requestCourse, int studentId) {
+    //requestCourseが、受講生詳細更新に必要なコースID,コース名,受講終了予定日を持っていない場合はNullPointerExceptionを投げる
+    if (requestCourse.getCourseId() == 0 |
+        requestCourse.getCourseName().isEmpty() |
+        requestCourse.getCourseEndAt() == null) {
+      throw new NullPointerException();
+
+    } else {
+      this.courseId = requestCourse.getCourseId();
+      this.courseName = requestCourse.getCourseName();
+      this.studentId = studentId;
+      this.courseStartAt = requestCourse.courseStartAt;
+      this.courseEndAt = requestCourse.getCourseEndAt();
+    }
+
   }
 
   @JsonCreator
