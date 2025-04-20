b417caf56097c6aebb2cac7502e8468a54be94bd
saway <sawa.y918@gmail.com>
2025-04-20 17:33:29 +0900

    refactor: Studentクラスからsetterメソッドを削除し、フィールドをすべてfinalに設定

--- src/main/java/raisetech/student/management/data/Student.java
+++ src/main/java/raisetech/student/management/data/Student.java
@@ -10,64 +10,62 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
 import lombok.Getter;
-import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
 import raisetech.student.management.data.domain.validation.OnUpdate;
 import raisetech.student.management.data.domain.validation.PhoneNumber;
 
 @Schema(description = "受講生")
 @Getter
-@Setter
 public class Student {
 
   @Schema(description = "受講生ID 自動採番を行う", example = "1")
   @Positive(groups = OnUpdate.class)
-  private int studentId;
+  private final int studentId;
 
   @Schema(description = "フルネーム", example = "佐藤花子")
   @NotNull
   @Length(max = 50)
-  private String fullname;
+  private final String fullname;
 
   @Schema(description = "かな", example = "さとうはなこ")
   @NotNull
   @Length(max = 50)
-  private String kanaName;
+  private final String kanaName;
 
   @Schema(description = "ニックネーム", example = "ハナ")
   @Length(max = 50)
-  private String nickname;
+  private final String nickname;
 
   @Schema(description = "Emailアドレス", example = "hanako@raisetech.com")
   @NotNull
   @Email
   @Length(max = 50)
-  private String email;
+  private final String email;
 
   @Schema(description = "居住地（都道府県）", example = "東京都")
   @Length(max = 50)
-  private String area;
+  private final String area;
 
   @Schema(description = "電話番号", example = "070-0000-0000")
   @PhoneNumber
-  private String telephone;
+  private final String telephone;
 
   @Schema(description = "年齢", example = "20")
   @Positive
   @Min(15)
   @Max(80)
-  private int age;
+  private final int age;
 
   @Schema(description = "性別 '男''女''その他'のみが入力可能", example = "女")
   @Pattern(regexp = "^(男|女|その他)$", message = "性別は「男」「女」「その他」のいずれかを入力してください")
-  private String sex;
+  private final String sex;
 
   @Schema(description = "備考", example = "転職活動中")
   @Length(max = 200)
-  private String remark;
+  private final String remark;
 
   @Schema(description = "キャンセルフラグ", example = "false")
-  private boolean isDeleted;
+  private final boolean isDeleted;
 
   @JsonCreator
   public Student(@JsonProperty("studentId") int studentId,
