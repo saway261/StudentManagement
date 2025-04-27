a4e42f86d314905c578b2230b0fa58701326832d
saway <sawa.y918@gmail.com>
2025-04-27 12:40:37 +0900

    feat: StudentControllerTestクラスにおいて、searchActiveStudentDetailListに対するテストメソッドを実装

--- src/test/java/raisetech/student/management/controller/StudentControllerTest.java
+++ src/test/java/raisetech/student/management/controller/StudentControllerTest.java
@@ -1,7 +1,6 @@
 package raisetech.student.management.controller;
 
 import static org.mockito.Mockito.times;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
@@ -12,6 +11,7 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import raisetech.student.management.exception.handling.ErrorDetailsBuilder;
 import raisetech.student.management.service.StudentService;
 
 @WebMvcTest(StudentController.class)
@@ -30,15 +30,65 @@ class StudentControllerTest {
     public StudentService service() {
       return Mockito.mock(StudentService.class);
     }
+
+    @Bean
+    public ErrorDetailsBuilder errorDetailsBuilder() {
+      return Mockito.mock(ErrorDetailsBuilder.class);
+    }
   }
 
   @Test
-  void アクティブ受講生の一覧検索ができて空のリストが返ってくること() throws Exception {
+  void アクティブ受講生一覧検索_サービスの処理が呼び出せていること() throws Exception {
     mockMvc.perform(MockMvcRequestBuilders.get("/students"))
-        .andExpect(status().isOk())
-        .andExpect(content().json("[]"));
+        .andExpect(status().isOk());
 
     Mockito.verify(service, times(1)).searchActiveStudentDetailList();
   }
-
+//
+//  @Test
+//  void 受講生詳細単一検索バリデーションエラー_受講生IDに数値以外を渡したときに入力チェックにかかること() {
+//    Student student = new Student();
+//  }
+//
+//  @Test
+//  void 受講生詳細単一検索成功_サービスの処理が適切に呼び出されStudentDetailが返ってくること()
+//      throws Exception {
+//    // 前提
+//    int studentId = 1;
+//
+//    mockMvc.perform(MockMvcRequestBuilders.get("/students/{studentId}"))
+//        .andExpect(status().isOk());
+//    Mockito.verify(service, times(1)).searchStudentDetail(studentId);
+//  }
+//
+//  @Test
+//  void 受講生詳細単一検索失敗_サービスから例外を受け取りそのまま投げていること() {
+//
+//  }
+//
+//  @Test
+//  void 古いエンドポイントに対するアクティブ受講生詳細一覧検索リクエスト_例外が返されること() {
+//
+//  }
+//
+//  @Test
+//  void 受講生詳細登録成功_サービスの処理を呼び出す際に適切なリクエストボディが渡されていること() {
+//
+//  }
+//
+//  @Test
+//  void 受講生詳細登録失敗_入力チェックにかかること() {
+//
+//  }
+//
+//  @Test
+//  void 受講生詳細更新成功_サービスの処理を呼び出す際に適切なリクエストボディが渡されていること() {
+//
+//  }
+//
+//  @Test
+//  void 受講生詳細更新失敗_入力チェックにかかること() {
+//
+//  }
+//
 }
