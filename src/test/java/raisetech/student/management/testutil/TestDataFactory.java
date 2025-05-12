ee82e336fc85019b526d40bf8a90db2a98826c4e
saway <sawa.y918@gmail.com>
2025-05-12 15:07:05 +0900

    feat: TestDataFactoryにおいて、makeDummyStudentDetailFormOnRegisterメソッドとmakeDummyStudentDetailFormOnUpdateメソッドを定義した

--- src/test/java/raisetech/student/management/testutil/TestDataFactory.java
+++ src/test/java/raisetech/student/management/testutil/TestDataFactory.java
@@ -6,6 +6,9 @@ import raisetech.student.management.data.Student;
 import raisetech.student.management.data.StudentCourse;
 import raisetech.student.management.data.domain.StudentDetail;
 import raisetech.student.management.data.value.Id;
+import raisetech.student.management.web.form.StudentCourseForm;
+import raisetech.student.management.web.form.StudentDetailForm;
+import raisetech.student.management.web.form.StudentForm;
 
 public class TestDataFactory {
 
@@ -27,5 +30,32 @@ public class TestDataFactory {
   public static StudentDetail makeCompletedStudentDetail(Id studentId, Id courseId) {
     return new StudentDetail(makeCompletedStudent(studentId),
         List.of(makeCompletedStudentCourse(studentId, courseId)));
+  }
+
+  public static StudentDetailForm makeDummyStudentDetailFormOnRegister() {
+    StudentForm studentForm = new StudentForm(
+        null, "山田太郎", "やまだたろう", "タロー", "taro@email.com",
+        "東京都練馬区", "090-0000-0000", 20, "男", "特になし", false
+    );
+    StudentCourseForm studentCourseForm = new StudentCourseForm(
+        null, "Javaコース", null
+    );
+
+    return new StudentDetailForm(studentForm, List.of(studentCourseForm));
+  }
+
+  public static StudentDetailForm makeDummyStudentDetailFormOnUpdate(Integer studentId,
+      Integer courseId) {
+    LocalDate now = LocalDate.now();
+
+    StudentForm studentForm = new StudentForm(
+        studentId, "山田太郎", "やまだたろう", "タロー", "taro@email.com",
+        "東京都練馬区", "090-0000-0000", 20, "男", "特になし", false
+    );
+    StudentCourseForm studentCourseForm = new StudentCourseForm(
+        courseId, "Javaコース", now.plusMonths(6)
+    );
+
+    return new StudentDetailForm(studentForm, List.of(studentCourseForm));
   }
 }
