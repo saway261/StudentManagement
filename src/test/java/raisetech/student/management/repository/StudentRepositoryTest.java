040c6b1f6a1ba44c5e087acb4ad1a85da28544ee
saway <sawa.y918@gmail.com>
2025-05-13 21:08:45 +0900

    feat: StudentRepositoryTestクラスを作成し、テストメソッド実装の準備をした

---
+++ src/test/java/raisetech/student/management/repository/StudentRepositoryTest.java
@@ -0,0 +1,18 @@
+package raisetech.student.management.repository;
+
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@MybatisTest
+class StudentRepositoryTest {
+
+  @Autowired
+  private StudentRepository sut;
+
+  @Test
+  void test() {
+
+  }
+
+}
