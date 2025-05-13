035a6376c8a662bd889a9f3e62a66bdab64c2b9e
saway <sawa.y918@gmail.com>
2025-05-13 22:02:12 +0900

    feat: StudentRepositoryTestにおいて、registerStudentメソッドのテストメソッドを実装

--- src/test/java/raisetech/student/management/repository/StudentRepositoryTest.java
+++ src/test/java/raisetech/student/management/repository/StudentRepositoryTest.java
@@ -1,6 +1,7 @@
 package raisetech.student.management.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static raisetech.student.management.testutil.TestDataFactory.makeCompletedStudent;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
@@ -18,6 +19,16 @@ class StudentRepositoryTest {
   void アクティブな受講生の全件検索が行えること() {
     List<Student> actual = sut.searchActiveStudentList();
     assertThat(actual.size()).isEqualTo(5);
+  }
+
+
+  @Test
+  void 受講生登録が行えること() {
+    sut.registerStudent(makeCompletedStudent(null));
+
+    List<Student> actual = sut.searchActiveStudentList();
+    assertThat(actual.size()).isEqualTo(6);
+
   }
 
 
