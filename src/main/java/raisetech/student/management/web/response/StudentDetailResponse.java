0ecaadd173a9b32e6b2fabb752d8dfb722f7b543
saway <sawa.y918@gmail.com>
2025-05-13 14:49:58 +0900

    StudentDetailResponseクラスにおいて、コンストラクタの引数をStudentDetail(ドメイン)にし、fromDomainメソッドはコンストラクタを利用するように修正

--- src/main/java/raisetech/student/management/web/response/StudentDetailResponse.java
+++ src/main/java/raisetech/student/management/web/response/StudentDetailResponse.java
@@ -3,14 +3,12 @@ package raisetech.student.management.web.response;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.ArrayList;
 import java.util.List;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import raisetech.student.management.data.StudentCourse;
 import raisetech.student.management.data.domain.StudentDetail;
 
 @Getter
 @Schema(description = "受講生詳細（出力用）")
-@AllArgsConstructor
 public class StudentDetailResponse {
 
   @Schema(description = "受講生情報")
@@ -19,14 +17,16 @@ public class StudentDetailResponse {
   @Schema(description = "受講生が登録しているすべてのコース情報")
   private final List<StudentCourseResponse> studentCourseList;
 
-  public static StudentDetailResponse fromDomain(StudentDetail domain) {
-    StudentResponse studentResponse = new StudentResponse(domain.getStudent());
-    List<StudentCourseResponse> studentCourseResponseList = new ArrayList<>();
-
+  private StudentDetailResponse(StudentDetail domain) {
+    this.student = new StudentResponse(domain.getStudent());
+    List<StudentCourseResponse> courseResponseList = new ArrayList<>();
     for (StudentCourse course : domain.getStudentCourseList()) {
-      studentCourseResponseList.add(new StudentCourseResponse(course));
+      courseResponseList.add(new StudentCourseResponse(course));
     }
+    this.studentCourseList = courseResponseList;
+  }
 
-    return new StudentDetailResponse(studentResponse, studentCourseResponseList);
+  public static StudentDetailResponse fromDomain(StudentDetail domain) {
+    return new StudentDetailResponse(domain);
   }
 }
