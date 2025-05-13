c6ad62aa0576dc7da4b7640161904cff07438c6d
saway <sawa.y918@gmail.com>
2025-05-13 13:20:24 +0900

    fix: StudentDetailResponseクラスに、fromDomainメソッドをstaticとして実装

--- src/main/java/raisetech/student/management/web/response/StudentDetailResponse.java
+++ src/main/java/raisetech/student/management/web/response/StudentDetailResponse.java
@@ -1,12 +1,16 @@
 package raisetech.student.management.web.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.ArrayList;
 import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import raisetech.student.management.data.StudentCourse;
 import raisetech.student.management.data.domain.StudentDetail;
 
 @Getter
 @Schema(description = "受講生詳細（出力用）")
+@AllArgsConstructor
 public class StudentDetailResponse {
 
   @Schema(description = "受講生情報")
@@ -15,10 +19,14 @@ public class StudentDetailResponse {
   @Schema(description = "受講生が登録しているすべてのコース情報")
   private final List<StudentCourseResponse> studentCourseList;
 
-  public StudentDetailResponse(StudentDetail domain) {
-    this.student = new StudentResponse(domain.getStudent());
-    this.studentCourseList = domain.getStudentCourseList().stream()
-        .map(StudentCourseResponse::new)
-        .toList();
+  public static StudentDetailResponse fromDomain(StudentDetail domain) {
+    StudentResponse studentResponse = new StudentResponse(domain.getStudent());
+    List<StudentCourseResponse> studentCourseResponseList = new ArrayList<>();
+
+    for (StudentCourse course : domain.getStudentCourseList()) {
+      studentCourseResponseList.add(new StudentCourseResponse(course));
+    }
+
+    return new StudentDetailResponse(studentResponse, studentCourseResponseList);
   }
 }
