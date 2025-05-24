738597736422a6104c4bb61c06d3d253ed906c68
saway <sawa.y918@gmail.com>
2025-05-24 15:55:06 +0900

    fix: StudentCourseにおいて、studentIdとcourseStartAt, courseEndAtをセットするビジネスロジックを持つコンストラクタを削除

--- src/main/java/raisetech/student/management/data/StudentCourse.java
+++ src/main/java/raisetech/student/management/data/StudentCourse.java
@@ -21,43 +21,5 @@ public class StudentCourse {
 
   private final LocalDate courseEndAt;
 
-  /**
-   * 受講生詳細登録で呼び出されるコンストラクタです。コース名,受講生IDを受け取り、コンストラクタが呼び出された日付をもとに、受講開始日と受講終了予定日を自動的にセットします。
-   * 受講生詳細更新で呼び出すと、courseIdがnullなのでのちの処理でInvalidIdExceptionが投げられます。これにより、受講生詳細更新で子のコンストラクタを呼び出し、意図せず受講終了予定日が書き替えられることを防ぎます。
-   *
-   * @param courseName コース名
-   * @param studentId  受講生ID
-   */
-  public StudentCourse(String courseName, Id studentId) {
-    this.courseId = null;
-    this.courseName = courseName;
-    this.studentId = studentId;
-
-    LocalDate now = LocalDate.now();
-    this.courseStartAt = now;
-    this.courseEndAt = now.plusMonths(6);
-  }
-
-  /**
-   * 受講生詳細更新で呼び出されるコンストラクタです。受講生コースオブジェクトと受講生IDを受け取ります。
-   * 受講生IDが0またはコース名,コース終了予定日がnullのStudentCourseオブジェクトを受け取った場合はNullPointerExceptionを投げます。
-   *
-   * @param requestCourse 受講生コースコースオブジェクト
-   * @param studentId     受講生ID
-   */
-  public StudentCourse(StudentCourse requestCourse, Id studentId) {
-    if (requestCourse.getCourseId() == null ||
-        requestCourse.getCourseName().isEmpty() ||
-        requestCourse.getCourseEndAt() == null) {
-      throw new NullPointerException();
-
-    } else {
-      this.courseId = requestCourse.getCourseId();
-      this.courseName = requestCourse.getCourseName();
-      this.studentId = studentId;
-      this.courseStartAt = requestCourse.courseStartAt;
-      this.courseEndAt = requestCourse.getCourseEndAt();
-    }
-  }
 
 }
