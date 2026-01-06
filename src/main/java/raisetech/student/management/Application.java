2821af46847f367ed786a937cb3c74ae6761c004
saway <sawa.y918@gmail.com>
2026-01-06 22:09:09 +0900

    first commit : localhost8080 にアクセスした際に Hello World! が表示される状態

---
+++ src/main/java/raisetech/student/management/Application.java
@@ -0,0 +1,22 @@
+package raisetech.student.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@GetMapping
+	public String hello(){
+		return "Hello, World!";
+	}
+
+}
