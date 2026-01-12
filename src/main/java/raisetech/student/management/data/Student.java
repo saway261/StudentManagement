8428b1c65df1e720bb45c5de5fbdf760b317a8a6
saway <sawa.y918@gmail.com>
2026-01-12 18:02:05 +0900

    feat: テーブルと紐づくStudentオブジェクト及びStudentCourseオブジェクトを定義

---
+++ src/main/java/raisetech/student/management/data/Student.java
@@ -0,0 +1,20 @@
+package raisetech.student.management.data;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Student {
+  private int studentId;
+  private String fullName;
+  private String kanaName;
+  private String nickname;
+  private String email;
+  private String area;
+  private String telephone;
+  private int age;
+  private String sex;
+  private String remark;
+
+}
