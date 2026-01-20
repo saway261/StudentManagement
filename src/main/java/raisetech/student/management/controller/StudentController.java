6f0796b49720355a1b1e909889c56dc6ee22fcfc
saway <sawa.y918@gmail.com>
2026-01-20 23:12:38 +0900

    feat: StudentController に @Validated を付与しバリデーション対象に設定

--- src/main/java/raisetech/student/management/controller/StudentController.java
+++ src/main/java/raisetech/student/management/controller/StudentController.java
@@ -1,8 +1,10 @@
 package raisetech.student.management.controller;
 
+import jakarta.validation.constraints.Positive;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,11 +12,14 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import raisetech.student.management.domain.StudentDetail;
+import raisetech.student.management.domain.validation.CreateGroup;
+import raisetech.student.management.domain.validation.UpdateGroup;
 import raisetech.student.management.service.StudentService;
 
 /**
  * 受講生の検索や登録、更新などを行うREST APIとして実行されるControllerです。
  */
+@Validated
 @RestController
 public class StudentController {
 
@@ -40,7 +45,7 @@ public class StudentController {
    * @return 受講生詳細
    */
   @GetMapping("/students/{studentId}")
-  public StudentDetail getStudent(@PathVariable int studentId){
+  public StudentDetail getStudent(@PathVariable @Positive int studentId){
     return service.searchStudentDetail(studentId);
   }
 
@@ -50,7 +55,7 @@ public class StudentController {
    * @return 登録後の受講生詳細
    */
   @PostMapping("/students")
-  public ResponseEntity<StudentDetail> registerStudent(@RequestBody StudentDetail studentDetail){
+  public ResponseEntity<StudentDetail> registerStudent(@RequestBody @Validated(CreateGroup.class) StudentDetail studentDetail){
     StudentDetail responseStudentDetail = service.registerStudentDetail(studentDetail);
     return ResponseEntity.ok(responseStudentDetail);
   }
@@ -61,7 +66,8 @@ public class StudentController {
    * @return 更新後の受講生詳細
    */
   @PutMapping("/students")
-  public ResponseEntity<StudentDetail> updateStudent(@RequestBody StudentDetail studentDetail){
+  @Validated(UpdateGroup.class)
+  public ResponseEntity<StudentDetail> updateStudent(@RequestBody @Validated(UpdateGroup.class) StudentDetail studentDetail){
     StudentDetail responseStudentDetail = service.updateStudentDetail(studentDetail);
     return ResponseEntity.ok(responseStudentDetail);
   }
