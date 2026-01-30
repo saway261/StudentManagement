bade06d8cc300cb507b5fca9bbda18b78425ae89
saway <sawa.y918@gmail.com>
2026-01-30 21:41:34 +0900

    docs: ErrorResponseオブジェクトにAPI仕様書を記載

--- src/main/java/raisetech/student/management/exception/handler/ErrorResponse.java
+++ src/main/java/raisetech/student/management/exception/handler/ErrorResponse.java
@@ -1,20 +1,32 @@
 package raisetech.student.management.exception.handler;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 import java.util.Map;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
-/**
- * エラーレスポンスの詳細を持つオブジェクトです
- */
+@Schema(description = "エラーレスポンス")
 @AllArgsConstructor
 @Getter
 public class ErrorResponse {
 
+  @Schema(description = "HTTPステータスコード", example = "400 BAD_REQUEST")
   private final HttpStatus status;
+
+  @Schema(description = "エラーの概要", example = "validation error")
   private final String message;
+
+  @Schema(description = "エラーの詳細", example = """
+        [
+                {
+                    "field": "getStudent.studentId",
+                    "message": "0 より大きな値にしてください"
+                }
+            ]
+      """
+  )
   private final List<Map<String, String>> errors;
 
 }
