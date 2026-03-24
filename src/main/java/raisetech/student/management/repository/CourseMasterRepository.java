695f0128c0da3382427b1bffc0a905b023829fcd
saway <sawa.y918@gmail.com>
2026-03-24 09:07:58 +0900

    feat: CourseMasterRepositoryクラスを作成し、指定されたコースコードがDBに存在するか判定する existsByCourseCode メソッドを実装

---
+++ src/main/java/raisetech/student/management/repository/CourseMasterRepository.java
@@ -0,0 +1,15 @@
+package raisetech.student.management.repository;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface CourseMasterRepository {
+
+  /**
+   * 指定されたコースコードがDBに存在するか確認する
+   * @return 存在すればtrue、しなければfalse
+   */
+  @Select("SELECT COUNT(*) > 0 FROM COURSE_MASTER WHERE course_code = #{courseCode}")
+  boolean existsByCourseCode(String courseCode);
+}
