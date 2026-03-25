1f05ea263e92f77beea607e53994ab3010430f5a
saway <sawa.y918@gmail.com>
2026-03-25 21:27:51 +0900

    feat: Courseオブジェクトに対し、バリデーションを設定

--- src/main/java/raisetech/student/management/data/master/Course.java
+++ src/main/java/raisetech/student/management/data/master/Course.java
@@ -1,13 +1,29 @@
 package raisetech.student.management.data.master;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 
+@Schema(description = "提供コース")
 @EqualsAndHashCode
 @AllArgsConstructor
 public class Course {
 
+  @Schema(description = "コースコード 英字大文字2~5文字", example = "JA")
+  @NotBlank
+  @Size(min = 2, max = 5)
+  @Pattern(
+      regexp = "^[A-Z]+$",
+      message = "コースコードは英字大文字のみで入力してください"
+  )
   private final String courseCode;
+
+  @Schema(description = "コース名" ,example = "Javaコース")
+  @NotBlank
+  @Size(max = 20)
   private final String courseName;
 
   public String getCourseCode() {
