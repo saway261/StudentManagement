2777025693d61d79c9ebf53a72c0f79821979d50
saway <sawa.y918@gmail.com>
2026-03-26 17:20:19 +0900

    docs: API仕様書の受講生管理システムの概要説明をアップデート コースも対象範囲に含めることを明示

--- src/main/java/raisetech/student/management/Application.java
+++ src/main/java/raisetech/student/management/Application.java
@@ -5,7 +5,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@OpenAPIDefinition(info = @Info(title = "受講生管理システム", description = "受講生及び受講コースの登録・更新・検索を行います。", version = "0.0"))
+@OpenAPIDefinition(info = @Info(title = "受講生管理システム", description = "受講生及び受講コースの登録・更新・検索を行います。また、提供しているコースの登録・更新・検索も行います。", version = "1.0"))
 @SpringBootApplication
 public class Application {
 
